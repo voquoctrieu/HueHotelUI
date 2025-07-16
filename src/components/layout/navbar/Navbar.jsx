c1cd0aa38e5540a68b3ce3f64e2e55e69e3774c4
voquoctrieu <22T1020774@husc.edu.vn>
2025-07-16 10:09:09 +0700

    Fix avatar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -190,10 +190,7 @@ function Navbar() {
                 <Box sx={{ flexGrow: 0 }}>
                   <Tooltip >
                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      <Avatar
-                        alt='Remy Sharp'
-                        src='/static/images/avatar/2.jpg'
-                      />
+                      <Avatar alt='Remy Sharp' />
                     </IconButton>
                   </Tooltip>
                   <Menu
